6723d3623a6a9268d612661ba605b308b4282e7c
harshmunshioxolo <h.munshi@oxolo.com>
2021-03-10 00:54:18 +0100

    working training

--- train.py
+++ train.py
@@ -1,26 +1,40 @@
-from data.dataloader import UTKDataset
-from torchvision import transforms, utils, models
-from torch.utils.data import DataLoader
-import torch.nn as nn
 import torch
+import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import random_split
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import models, transforms, utils
 from tqdm import tqdm
 
-if __name__=="__main__":
+from data.dataloader import UTKDataset
+
+if __name__ == "__main__":
+
+    # -2: initiaze the summarywriter
+    writer = SummaryWriter()
 
     # -1: Define if device is cuda
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # 0. define the network.
-    net = models.resnet18(pretrained=True)
+    net = models.resnet50(pretrained=True)
 
     # 1. change the final layer
-    net.fc = nn.Linear(in_features=512, out_features=7, bias=True)
+    net.fc = nn.Linear(in_features=2048, out_features=7, bias=True)
     net.to(device=device)
 
-    # 2. Define the dataloader
+    # 2a. Define the dataloader
     utkdataset = UTKDataset(root_dir="/home/harsh/Documents/github_projects/face_data")
-    UTKLoader = DataLoader(utkdataset, batch_size=16, shuffle=True, num_workers=4)
+    val_data = int(len(utkdataset) * 0.2)
+    train_data_len = len(utkdataset) - val_data
+
+    # 2b. Split the data in train and test
+    train_data, test_data = random_split(utkdataset, [train_data_len, val_data])
+    train_loader, val_loader = (
+        DataLoader(train_data, batch_size=16, shuffle=True, num_workers=4),
+        DataLoader(val_data, batch_size=1, shuffle=False, num_workers=4),
+    )
 
     # 3. Identify the loss functions
     # The output layer consists of 8 outputs. 5 for race, 2 gender, 1 age = 8
@@ -37,11 +51,13 @@ if __name__=="__main__":
 
     # 5. Activate sigmoid
     sig = nn.Sigmoid()
+    writer.add_graph(net, torch.randn(1, 3, 224, 224).to(device=device))
 
     # 5. Setup the net to train
     net.train()
-    for epoch in tqdm(range(2)):
-        for i, data in tqdm(enumerate(UTKLoader)):
+    for epoch in tqdm(range(50)):
+        print(epoch)
+        for i, data in tqdm(enumerate(train_loader)):
             inputs = data["image"].to(device=device)
             age_label = data["age"].to(device=device)
             gender_label = data["gender"].to(device=device)
@@ -51,12 +67,39 @@ if __name__=="__main__":
             optimizer.zero_grad()
             outputs = net(inputs)
             loss_1 = criterion_1(outputs[:, 0:5], race_label)
-            loss_2 = criterion_2(sig(outputs[:, 5:6]), gender_label.unsqueeze(1).float())
+            loss_2 = criterion_2(
+                sig(outputs[:, 5:6]), gender_label.unsqueeze(1).float()
+            )
             loss_3 = criterion_3(outputs[:, 6], age_label.float())
             loss = loss_1 + loss_2 + loss_3
-
+            writer.add_scalar('Loss/Ethnicity', loss_1.item(), epoch*train_data_len+i)
+            writer.add_scalar('Loss/Gender', loss_2.item(), epoch*train_data_len+i)
+            writer.add_scalar('Loss/Age', loss_3.item(), epoch*train_data_len+i)
+            writer.add_scalar('Loss/total_loss', loss.item(), epoch*train_data_len+i)
             loss.backward()
             optimizer.step()
-    
+
+            # enter validation at evey 5 epochs
+            if epoch+1 % 5 == 0:
+                # save the model
+                for j, data in tqdm(enumerate(val_loader)):
+                    inputs = data["image"].to(device=device)
+                    age_label = data["age"].to(device=device)
+                    gender_label = data["gender"].to(device=device)
+                    race_label = data["race"].to(device=device)
+
+                    output = net(input)
+
+                    loss_1 = criterion_1(outputs[:, 0:5], race_label)
+                    loss_2 = criterion_2(
+                        sig(outputs[:, 5:6]), gender_label.unsqueeze(1).float()
+                    )
+                    loss_3 = criterion_3(outputs[:, 6], age_label.float())
+                    loss = loss_1 + loss_2 + loss_3
+                    writer.add_scalar('Val/Ethnicity', loss_1.item(), j)
+                    writer.add_scalar('Val/Gender', loss_2.item(), j)
+                    writer.add_scalar('Val/Age', loss_3.item(), j)
+                    writer.add_scalar('Val/total_loss', loss.item(), j)
+
     PATH = "./UTKFaceCNN.pth"
     torch.save(net.state_dict(), PATH)
